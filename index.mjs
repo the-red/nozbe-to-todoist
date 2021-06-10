1d1e7651ce2c654ae6f08ead427e84fa0103c799
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-06-10 13:29:18 +0900

    feat: project, taskの2つCSV出力

--- index.mjs
+++ index.mjs
@@ -1,8 +1,11 @@
 import stringify from 'csv-stringify/lib/sync.js';
 import json from './data.json';
-import fs from 'fs'
+import fs from 'fs';
 
-const outputString = stringify(json.project, { header: true, bom: true });
+fs.mkdirSync('dist', { recursive: true });
 
-console.log(outputString);
-fs.writeFileSync('a.csv', outputString)
+const project = stringify(json.project, { header: true, bom: true });
+const task = stringify(json.task, { header: true, bom: true });
+
+fs.writeFileSync(`dist/project.csv`, project);
+fs.writeFileSync(`dist/task.csv`, task);

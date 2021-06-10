72cb29c8ac4319f73fd9e0094e949fc74c4e9e4f
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-06-10 16:03:15 +0900

    feat: Nozbe -> Todoistの形式にプロジェクトごとに変換できるように

--- index.mjs
+++ index.mjs
@@ -6,6 +6,28 @@ fs.mkdirSync('dist', { recursive: true });
 
 const project = stringify(json.project, { header: true, bom: true });
 const task = stringify(json.task, { header: true, bom: true });
-
 fs.writeFileSync(`dist/project.csv`, project);
 fs.writeFileSync(`dist/task.csv`, task);
+
+json.project.forEach((project) => {
+  project.id, project.name;
+
+  const taskObj = json.task
+    .filter((_) => _.project_id === project.id)
+    .map((_) => {
+      return {
+        TYPE: 'task',
+        CONTENT: _.name,
+        DESCRIPTION: _.comments.map((_) => _.body).join('\n'),
+        PRIORITY: _.next || '4',
+        INDENT: '1',
+        AUTHOR: '',
+        RESPONSIBLE: '',
+        DATE: _.datetime,
+        DATE_LANG: '',
+        TIMEZONE: '',
+      };
+    });
+  const todoist = stringify(taskObj, { header: true, bom: true });
+  fs.writeFileSync(`dist/todoist-${project.name}.csv`, todoist);
+});
